40eb79a1016eeff585647b1e0a487dcd809781b2
Laurence <lggousie46@gmail.com>
2026-05-10 19:08:59 +0200

    render-core: add namespace wrapper root

--- src/howl_render.zig
+++ src/howl_render.zig
@@ -4,23 +4,13 @@
 
 const lib = @This();
 const std = @import("std");
-const core = @import("render_core.zig").RenderCore;
-const ffi = @import("ffi.zig");
-const renderer = @import("renderer.zig");
+const render = @import("render/main.zig");
+const ffi = render.c_api;
 
-pub const Core = core;
+pub const Core = render.Core;
 pub const Ffi = ffi;
-pub const Renderer = renderer.Renderer;
-
-pub const geometry = struct {
-    pub fn deriveGridSize(grid_px: Core.PixelSize, cell_px: Core.CellSize) Core.GridSize {
-        return Core.deriveGridSize(grid_px, cell_px);
-    }
-
-    pub fn deriveGridForFrame(render_px: Core.PixelSize, grid_px: Core.PixelSize, cell_px: Core.CellSize) Core.FrameGeometryError!Core.GridSize {
-        return Core.deriveGridForFrame(render_px, grid_px, cell_px);
-    }
-};
+pub const Renderer = render.Renderer;
+pub const geometry = render.geometry;
 
 comptime {
     if (@import("root") == lib) {
---
+++ src/render/main.zig
@@ -0,0 +1,25 @@
+//! Render namespace wrapper for the howl-render-core module.
+
+const std = @import("std");
+
+pub const c_api = @import("../ffi.zig");
+
+const core = @import("../render_core.zig").RenderCore;
+const renderer = @import("../renderer.zig");
+
+pub const Core = core;
+pub const Renderer = renderer.Renderer;
+
+pub const geometry = struct {
+    pub fn deriveGridSize(grid_px: Core.PixelSize, cell_px: Core.CellSize) Core.GridSize {
+        return Core.deriveGridSize(grid_px, cell_px);
+    }
+
+    pub fn deriveGridForFrame(render_px: Core.PixelSize, grid_px: Core.PixelSize, cell_px: Core.CellSize) Core.FrameGeometryError!Core.GridSize {
+        return Core.deriveGridForFrame(render_px, grid_px, cell_px);
+    }
+};
+
+test {
+    std.testing.refAllDecls(@This());
+}
